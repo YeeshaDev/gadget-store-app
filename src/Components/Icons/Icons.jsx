a7f03f60d83f7d2840d51d4781fd81c5794eaeca
YeeashaDev <aishaagunbiade05@gmail.com>
2023-01-13 21:06:36 +0100

    still ongoing

---
+++ src/Components/Icons/Icons.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import './Icons.css'
+
+
+function Icons() {
+    return (
+        <div className='icons-content'>
+            <Marquee>
+            <div className="marquee-icons">
+             <div className="image">
+         <img src='./assets/shipping-truck.svg' alt='' className='icons'/>
+         </div>
+         <div>
+            <h3>Free Delivery</h3>
+            <p>Free shipping on all order</p>
+            </div>
+            
+         </div>
+         
+         <div className="marquee-icons">
+<div className="image">
+         <img src='./assets/chat-support.svg' alt='' className='icons'/>
+         </div>
+         <div>
+            <h3>Online Support 24/7</h3>
+            <p>Support online 24 hours a day</p>
+            </div>
+            
+         </div>
+         <div className="marquee-icons">
+<div className="image">
+         <img src='./assets/returns.svg' alt='' className='icons return'/>
+         </div>
+         <div>
+            <h3>Money Return</h3>
+            <p>Guaranteed under 7 days</p>
+            </div>
+            
+         </div>
+         <div className="marquee-icons">
+            <div className="image">
+         <img src='./assets/discount.svg' alt='' className='icons'/>
+         </div>
+         <div>
+            <h3>Member Discount</h3>
+            <p>On every order under $100</p>
+            </div>
+            
+         </div>
+         </Marquee>
+        </div>
+    )
+}
+
+export default Icons
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header/Header';
 import Collection from '../Components/Collection/Collection';
+import Icons from '../Components/Icons/Icons';
 
 function Home() {
   return (
     <div>
         <Header/>
         <Collection/>
+        <Icons/>
     </div>
   )
 }
